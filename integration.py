58d4e2d24542e68ee60f283139a021857df22c1a
Caio Martins Sanchez <caiosanchgez29@gmail.com>
2024-03-06 14:35:14 -0500

    filtering

--- integration.py
+++ integration.py
@@ -10,14 +10,14 @@ Notes:
 
 - Integration from two file is DONE.
 
-- Next Goal 1: Is to dont let the program crash at the end, after the last board
+- Next Goal 1: Is to dont let the program crash at the end, after the last board.
 
-- Next Goal 2: Filter the boards to only get projects.
+- Next Goal 2: Filter the boards to only get projects.   DONE
 
 '''
 index2 = 0
 
-# Function to fetch the board`s name and ID from Monday.com's API
+# Function to fetch the board`s name and ID from Monday.com's API.
 def fetch_boardsID_from_monday(api_key):
     apiUrl = 'https://api.monday.com/v2/'
     headers = {'Authorization': api_key }
@@ -44,7 +44,7 @@ def fetch_boardsID_from_monday(api_key):
         print("Failed to retrieve data from Monday.com API. Status code:", response.status_code)
         return None
 
-# Function to organize the boards data from monday.com
+# Function to organize the boards data from monday.com.
 def organize_boardsID_from_monday(data):
     folders = data.get('data', {}).get('folders', [])
     boards_IDs = []
@@ -90,46 +90,52 @@ def print_out_data_from_monday(data):
     boards = data.get('data', {}).get('boards', [])
     global index2
 
+    # This statement will loop through all the boards and filter them.
     for board in boards:
         board_id = board.get('id')
         board_name = board.get('name')
         abbreviation = board_name[:4]
         full_name = board_name[7:]
         board_description = board.get('description')
-        index2 += 1
+        board_name_check = board_name[4]
 
-        # Slicing the Customer name from board description
-        if board_description:
+        # Check if the board is a project.
+        if (board_name_check == " ") or (board_name_check == "-"):
+            index2 += 1
+
+            # Slicing the Customer name from board description.
             start_phrase = "Owner: "
             end_phrase = "Customer: "
             start_index = board_description.find(start_phrase)
             end_index = board_description.find(end_phrase)
             owner_name = board_description[start_index + len(start_phrase):end_index].strip()
-            print(f"\nBoard: {abbreviation}\nBoard Name: {full_name}\nBoard ID: {board_id}\nOwner Name: {owner_name}\nProject Number: {index2}\n")
+                
+            if end_index:
+                print(f"\nBoard: {abbreviation}\nBoard Name: {full_name}\nProject Number: {index2}\nBoard ID: {board_id}\nOwner Name: {owner_name}\n")
         
-        else:
-            board_description = "No description for this one."
-            print(f"\nBoard: {abbreviation}\nBoard Name: {full_name}\nBoard Description: {board_description}\nProject Number: {index2}\n")
+            else:
+                board_description = "No description for this one."
+                print(f"\nBoard: {abbreviation}\nBoard Name: {full_name}\nBoard Description: {board_description}\nProject Number: {index2}\n")
 
         
 
-# Main function
+# Main function.
 def main():
-    # Your Monday.com API key
+    # Your Monday.com API key.
     api_key = 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjMxNTI1MjYyNiwiYWFpIjoxMSwidWlkIjo0NDc2ODk2MiwiaWFkIjoiMjAyNC0wMS0yOVQxNTo0NDoyMS4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6OTY4Mzg5MCwicmduIjoidXNlMSJ9.-jN2Xic1xNY9kVlr8XRYFTfy2gOLaVu9Gwp5LLfTCLk'
 
-    # Retrieve the boards` ID from Monday.com's API
+    # Retrieve the boards` ID from Monday.com's API.
     monday_data = fetch_boardsID_from_monday(api_key)
 
     if monday_data:
-        # Organize the board`s ID data from monday.com
+        # Organize the board`s ID data from monday.com.
         boards_IDs = organize_boardsID_from_monday(monday_data)
 
-        # Pull boards information with the IDs and print out
+        # Pull boards information with the IDs and print out.
         data = fetch_data_from_monday(api_key, boards_IDs)
 
         print_out_data_from_monday(data)
 
-# Entry point of the script
+# Entry point of the script.
 if __name__ == "__main__":
     main()
